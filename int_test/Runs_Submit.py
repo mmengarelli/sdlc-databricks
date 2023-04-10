5d771aeed7095e33e0288f3a96cebe311884d569
mengam01@gmail.com <mengam01@gmail.com>
2023-04-10 18:40:22 +0000

    Adding runs_submit

--- int_test/Runs_Submit.py
+++ int_test/Runs_Submit.py
@@ -1,2 +1,16 @@
 # Databricks notebook source
+# MAGIC %pip install databricks-cli
 
+# COMMAND ----------
+
+# In CI/CD these would be CLI commands
+
+from databricks_cli.sdk.api_client import ApiClient
+from databricks_cli.jobs.api import JobsApi
+
+client = ApiClient(
+  host  = "https://adb-7954466279135483.3.azuredatabricks.net/",
+  token = "" # Make this a secret
+)
+
+jobs_api = JobsApi(client)
